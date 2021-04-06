cfa2d94f0eaed4b5d97ab1c2ff2fde928b1d23e0
Iryna Alchynska <68770490+ira-alchynska@users.noreply.github.com>
2021-04-06 22:15:01 +0300

    added initial states

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import ContactList from './Component/ContactList';
 import Filter from './Component/Filter';
 import ContactForm from './Component/ContactForm';
 
+const INITIAL_STATE = {
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: '',
+};
+
 class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+  state = INITIAL_STATE;
 
   handleSubmitForm = contact => {
     const includedInContacts = this.state.contacts.find(
